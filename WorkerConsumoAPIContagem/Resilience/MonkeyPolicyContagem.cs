a66a438b8817a35f777df966660f2883e86b3ee3
Renato Groffe <renato.groffe@yahoo.com.br>
2021-09-29 15:45:13 -0300

    .NET 5 + Consumo de API REST de contagem de acessos + Código Resiliente com Polly + Fallback + Simulação de caos com Simmy
    .NET 5 + Consumo de API REST de contagem de acessos + Código Resiliente com Polly + Fallback + Simulação de caos com Simmy

---
+++ WorkerConsumoAPIContagem/Resilience/MonkeyPolicyContagem.cs
@@ -0,0 +1,18 @@
+using System;
+using Polly.Contrib.Simmy;
+using Polly.Contrib.Simmy.Outcomes;
+
+namespace WorkerConsumoAPIContagem.Resilience
+{
+    public static class MonkeyPolicyContagem
+    {
+        public static AsyncInjectOutcomePolicy CreateMonkeyPolicy()
+        {
+            // Criação da Chaos Policy com uma probabilidade
+            // de 30% de erro
+            return MonkeyPolicy.InjectExceptionAsync(
+                with => with.Fault(new Exception("Erro gerado em simulação de caos com Simmy..."))
+                    .InjectionRate(0.30).Enabled(true));
+        }
+    }
+}
